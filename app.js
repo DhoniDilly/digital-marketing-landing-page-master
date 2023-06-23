ea56152beffda44dbc72fb60ad6db2be6c9694bd
DhoniDilly <dilly0141@gmail.com>
2023-06-23 13:45:58 +0530

    Initial commit

---
+++ app.js
@@ -0,0 +1,17 @@
+
+let faqs = [document.querySelectorAll('.faq')];
+
+// faqs.map( faq {
+//     let ques = faq.querySelector('.question-box');
+
+//     ques.addEventListener('click'(){
+//         faq.classList.toggle('active');
+//     })
+// })
+
+ let cli = document.getElementById('.question-box');
+
+
+ cli.addEventListener('click', function(){
+    alert 'kjd';
+ })
